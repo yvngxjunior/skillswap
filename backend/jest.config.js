1f22fa926f307fdc3166025031c58b88e6bb5dd2
ostrolawzyy-beep <ostrolawzyy@gmail.com>
2026-04-08 14:03:10 +0200

    fix(conflicts): merge jest.config.js - combine globalSetup/teardown with coverage config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,8 +1,22 @@
+'use strict';
+
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.js'],
   globalSetup: './src/__tests__/setup.js',
   globalTeardown: './src/__tests__/teardown.js',
+  forceExit: true,
+  detectOpenHandles: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/database/migrate.js',
+    '!src/database/seed.js',
+    '!src/server.js',
+  ],
+  coverageThreshold: {
+    global: { lines: 80, functions: 80, branches: 70, statements: 80 },
+  },
   testTimeout: 15000,
   verbose: true,
 };
